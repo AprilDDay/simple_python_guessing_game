974ab79c3cb930f2a0cf31c33c1d1716d6fd96b0
April Day <april.day@gmail.com>
2021-08-20 23:23:56 +0100

    finish loop for guesing game

--- game.py
+++ game.py
@@ -6,5 +6,18 @@ player_name = input("Hello! What's your name?")
 
 print('Okay! ' + player_name + 'I am guessing a number between 1 and 10.')
 
+number_of_guesses = 0
+
 while number_of_guesses < 5:
-    guess = 
+    guess = int(input())
+    number_of_guesses += 1
+    if guess < number: 
+        print('Try again! Your guess is too low.')
+    if guess > number: 
+        print('Almost! Your guess is too high...')
+    if guess == number: 
+        print('You guessed the number in ' + str(number_of_guesses) + ' tries!')
+    else:
+        print('Bummer! You did NOT guess the number. It was ' + str(number))
+        
+
